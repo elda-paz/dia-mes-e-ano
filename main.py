9d8de61ea7d3f95879efe4e7d282b96d63836016
Elda Oliveira <eldaoliver.paz@gmail.com>
2023-03-07 23:43:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+nome = input ('Qual é o seu nome?')
+print ('Olá', nome,'! Prazer em te conhecer!')
+
+dia = input ('Qual é o dia do seu anivesario?')
+mes = input ('Qual é o mês do seu aniverario?')
+ano = input ('Qual é o ano do seu aniverario?')
+
+print('Você nasceu no dia', dia ,'de', mes ,'de', ano,'.', 'Correto?')
